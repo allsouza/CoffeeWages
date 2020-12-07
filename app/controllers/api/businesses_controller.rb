d7804fe0815039895e2d398d03cdf6cc0c3898c9
Drew <drewwebs@gmail.com>
2020-12-07 13:46:59 -0500

    Routes added

--- app/controllers/api/businesses_controller.rb
+++ app/controllers/api/businesses_controller.rb
@@ -1,2 +1,8 @@
 class Api::BusinessesController < ApplicationController
+
+
+    private
+    def business_params
+        params.require(:business).permit(:name, :address, :coordinates)
+    end
 end
