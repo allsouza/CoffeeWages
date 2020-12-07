920f4d97c62b1726e2373839bbefdcec45da741f
Brad Larson <Larsonbrad@protonmail.com>
2020-12-07 13:02:50 -0500

    splash first push

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Splash from './splash/splash'
+import { Route } from 'react-router-dom';
 
 export default function App() {
     return(
-        <h1>Our app is running</h1>
+        // <h1>Our app is running</h1>
+        <Route exact path="/" component={Splash} />
     )
 }
