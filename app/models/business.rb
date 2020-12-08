ca491c6136bcd2ab5db76b7c828770de1c86b019
Drew <drewwebs@gmail.com>
2020-12-08 17:18:28 -0500

    Null constraint removed from location column

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,5 +1,5 @@
 class Business < ApplicationRecord
-    validates :name, :location, presence:true
+    validates :name, presence:true
     validates :name, :address, :coordinates, uniqueness:true, allow_nil:true
 
     has_many :reviews
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_182318) do
+ActiveRecord::Schema.define(version: 2020_12_08_221730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_12_08_182318) do
     t.string "coordinates"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "location", null: false
+    t.string "location"
     t.index ["name"], name: "index_businesses_on_name"
   end
 
