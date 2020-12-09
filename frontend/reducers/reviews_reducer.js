ca65d48f0658fc36223c6fa533b3291c6801d846
Drew <drewwebs@gmail.com>
2020-12-09 10:18:48 -0500

    Reviews reducer

--- frontend/reducers/reviews_reducer.js
+++ frontend/reducers/reviews_reducer.js
@@ -1,14 +1,20 @@
-import { RECEIVE_ALL_REVIEWS } from "../actions/review_actions";
+import { DELETE_REVIEW, RECEIVE_ALL_REVIEWS } from "../actions/review_actions";
 
 export default function reviewsReducer(state={}, action) {
-    Object.freeze(state)
+    Object.freeze(state);
     let newState;
 
     switch (action.type) {
         case RECEIVE_ALL_REVIEWS:
             action.reviews.map(review => newState[review.id] = review)
-            return newState           
+            return newState;
+        case RECEIVE_REVIEW:
+            return Object.assign({}, state, {[action.review.id]: action.review});
+        case DELETE_REVIEW:
+            newState = Object.assign({}, state);
+            delete newState[action.reviewId];
+            return newState;
         default:
-            return state
+            return state;
     }
 }
