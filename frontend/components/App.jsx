7ad663c1e2cd969498f1d9afce8811d5461054d9
Brad Larson <larsonbrad@pm.me>
2020-12-09 17:08:38 -0500

    splash styling

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Splash from './splash/splash'
 import { Route, Switch } from 'react-router-dom';
 import Form from './reviews/Form';
+import Nav from './nav/nav'
 
 export default function App() {
   return (
     <Switch>
         <Route path='/new_review' component={Form} />
         <Route path='/' component={Splash}/>
+        <Route exact path="/" component={Nav} />
     </Switch>
   );
 }
