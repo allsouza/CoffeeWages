80b21b2b3d63fb5a4c9aa804140d8da09b8936a8
Drew <drewwebs@gmail.com>
2020-12-15 12:40:40 -0500

    Added search to review page

--- frontend/components/reviews/Index.jsx
+++ frontend/components/reviews/Index.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
+import { Button, TextField } from '@material-ui/core';
 import Review from './Show';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAllReviews } from '../../actions/review_actions';
@@ -6,16 +7,27 @@ import { fetchAllReviews } from '../../actions/review_actions';
 export default function ReviewIndex() {
     const reviews = Object.values(useSelector(({entities}) => entities.reviews));
     const dispatch = useDispatch();
+    const [name, setName] = useState("");
+    const [address, setAddress] = useState("");
 
-    useEffect(() => {
-        async function loadContent() {
-            await dispatch(fetchAllReviews({ filters: { } }));
+    function handleSubmit(e) {
+        e.preventDefault();
+        
+        if (name || address) {
+            dispatch(fetchAllReviews({filters: {name, address} }));
         }
-
-        loadContent();
-    }, []);
+    }
 
     return (
-        reviews ? reviews.map(review => <Review review={review} />) : <div>Loading...</div>   
+        <div className="reviews-index">
+            <div className='search-bar'>
+                <form onSubmit={handleSubmit} className='fields'>
+                    <TextField value={name} onChange={e => setName(e.target.value)} label="Business name" />
+                    <TextField value={address} onChange={e => setAddress(e.target.value)} label="Address" />
+                    <Button type="submit" variant='contained' size="medium" color="primary">Search</Button>
+                </form>
+            </div>
+            {reviews ? reviews.map(review => <Review review={review} />) : <div>Loading...</div>   }
+        </div>
     )
 }
