b5fac8ae1f71bf5b1782fa1f5e278c920c5544c8
Andre <andre.lls@hotmail.com>
2020-12-15 23:43:23 -0500

    Set up SendGrid mailer to send emails

--- app/controllers/api/errors_controller.rb
+++ app/controllers/api/errors_controller.rb
@@ -2,7 +2,7 @@ class Api::ErrorsController < ApplicationController
     protect_from_forgery with: :null_session #disable auth token
 
     def create
-        ErrorMailer.bug_report(params[:subject], params[:body]).deliver_now
-        render json: {success: true}
+        response = ErrorMailer.bug_report(params[:subject], params[:body])
+        render json: response.status_code
     end
 end
--- app/mailers/error_mailer.rb
+++ app/mailers/error_mailer.rb
@@ -1,10 +1,20 @@
-class ErrorMailer < ApplicationMailer
-    default from: 'system@coffeewages.com'
+require 'sendgrid-ruby'
+include SendGrid
 
-    def bug_report(subject, body)
-        mail(to: 'andre.llsouza8@gmail.com', 
-            subject: subject,
-            content_type: 'text/html',
-            body: body)
+class ErrorMailer
+    def self.bug_report(subject, body)
+        mail = SendGrid::Mail.new
+        mail.from = Email.new(email: 'andre.lls@hotmail.com')
+        mail.subject = subject
+        mail.add_content(Content.new(type: 'text/plain', value: body))
+        
+        personalization = Personalization.new
+        personalization.add_to(Email.new(email: 'andre.llsouza8@gmail.com', name: "Andre"))
+        personalization.add_to(Email.new(email: 'andriw.luiz@gmail.com', name: "Andre Souza"))
+        mail.add_personalization(personalization)
+
+        debugger
+        sg = SendGrid::API.new(api_key: Figaro.env.SENDGRID_API_KEY)
+        sg.client.mail._('send').post(request_body: mail.to_json)
     end
 end
