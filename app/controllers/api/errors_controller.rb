fe225060dbb1710ebaa2497e552d8038cf25d181
Andre Souza <andre.llsouza8@gmail.com>
2020-12-17 12:21:13 -0500

    Update errors_controller.rb
    remove debuggers

--- app/controllers/api/errors_controller.rb
+++ app/controllers/api/errors_controller.rb
@@ -2,7 +2,6 @@ class Api::ErrorsController < ApplicationController
     protect_from_forgery with: :null_session #disable auth token
 
     def create
-        debugger
         response = ErrorMailer.bug_report(params[:subject], params[:body])
         render json: response.status_code
     end
